f0b645e4a4663d4371f819d3e1086197f41b11fd
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-24 14:09:36 -0700

    Button, title and date finished

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ app.set('view engine', 'ejs') //writing all views with ejs
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-  res.render('index', { text: 'Hello!' })
+  const articles = [{
+    title: 'Test article',
+    createdAt: new Date(),
+    description: 'Test description'
+  },
+  {
+    title: 'Test article 2',
+    createdAt: new Date(),
+    description: 'Test description 2'
+  }]
+  res.render('index', { articles: articles })
 })
 
 app.listen(5000)
