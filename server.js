2b6c154a006f8ce367f77fe5f967134bde1f81c9
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-24 13:25:26 -0700

    index view

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs') //writing all views with ejs
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 app.listen(5000)
