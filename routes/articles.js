3a884159bd0e0ca4beae671e03aef3f7adcbde32
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-24 14:50:03 -0700

    Created article schema

--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,19 @@
 // This page renders out a list of our articles
 
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+
+mongoose.connect('mongodb://localhost/blog', { 
+  useUnifiedTopology: true, useNewUrlParser: true 
+})
 
 router.get('/new', (req, res) => {
   res.render('articles/new')
 })
 
 router.post('/', (req, res) => {
-  
+
 })
+
 module.exports = router
