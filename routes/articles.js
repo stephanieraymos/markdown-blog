d4d98e45eb09755d70b8a7124d7406752ee4e0a5
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-24 18:29:55 -0700

    Updates to form

--- routes/articles.js
+++ routes/articles.js
@@ -13,12 +13,14 @@ router.get('/new', (req, res) => {
   res.render('articles/new', { article: new Article() }) 
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  if (article == null) res.redirect('/')
+  res.render('articles/show', { article: article })
 })
 
 router.post('/', async (req, res) => {
-  const article = new Article({
+  let article = new Article({
     title: req.body.title,
     description: req.body.description,
     markdown: req.body.markdown
@@ -27,6 +29,7 @@ router.post('/', async (req, res) => {
     article = await article.save(); //saving new article + giving article id
     res.redirect(`/articles/${article.id}`)
   } catch (err){
+    console.log(err)
     res.render('articles/new', { article: article })
   }
 })
