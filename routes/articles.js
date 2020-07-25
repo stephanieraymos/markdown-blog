63933aaf994af853eaadb8447e9f9363c6b32d02
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-24 19:46:48 -0700

    Method override

--- routes/articles.js
+++ routes/articles.js
@@ -9,7 +9,7 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:slug', async (req, res) => {
-  const article = await Article.find({ slug: req.params.slug })
+  const article = await Article.findOne({ slug: req.params.slug })
   if (article == null) res.redirect('/')
   res.render('articles/show', { article: article })
 })
@@ -22,11 +22,16 @@ router.post('/', async (req, res) => {
   })
   try {
     article = await article.save(); //saving new article + giving article id
-    res.redirect(`/articles/${article.id}`)
+    res.redirect(`/articles/${article.slug}`)
   } catch (err){
     console.log(err)
     res.render('articles/new', { article: article })
   }
+})
+
+router.delete('/:id', async (req, res) =>{
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles');
-const app = express();
 const { MONGO_URI } = require('./config')
+const methodOverride = require('method-override') //overrides the method that form passes
+const app = express();
 
 mongoose.connect(MONGO_URI, {   
   useCreateIndex: true,
   useFindAndModify: false,
   useNewUrlParser: true,
-  useUnifiedTopology: true })
+  useUnifiedTopology: true
+ })
 
 app.set('view engine', 'ejs') //writing all views with ejs
 
 app.use(express.urlencoded({ extended: false })) //Access all parameters from article form in our article route by accessing req.body.parameterName
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' }) //grabbing every single article + sorting in descending order
