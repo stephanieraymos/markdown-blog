fb8729a4cc48ede49221f36cef5919b4aff6c71f
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-24 19:12:41 -0700

    Read more button + slug in article schema

--- models/article.js
+++ models/article.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const marked = require('marked'); // To create markdown and turn it into html
+const slugify = require('slugify'); // To convert something like a title into a url friendly slug
 const articleSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -15,6 +16,11 @@ const articleSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true
   }
 })
 
